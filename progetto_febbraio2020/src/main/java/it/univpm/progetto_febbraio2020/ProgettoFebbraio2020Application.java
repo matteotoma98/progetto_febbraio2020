dcb3f754a49a7f7c7023fc08c2c3fe1d4797067a
matteotoma98 <matteotoma98@hotmail.it>
2020-02-10 16:23:47 +0100

    prova

---
+++ progetto_febbraio2020/src/main/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020Application.java
@@ -0,0 +1,13 @@
+package it.univpm.progetto_febbraio2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFebbraio2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoFebbraio2020Application.class, args);
+	}
+
+}
---
+++ progetto_febbraio2020/src/test/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.progetto_febbraio2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFebbraio2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
