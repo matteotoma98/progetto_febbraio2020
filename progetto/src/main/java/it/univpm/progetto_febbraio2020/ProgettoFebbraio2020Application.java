988c746f59f5ff794d99c48fb0726eba7f658d23
matteotoma98 <matteotoma98@hotmail.it>
2020-02-11 15:44:08 +0100

    Creazione classe per springboot

--- progetto/src/main/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020Application.java
+++ progetto/src/main/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020Application.java
@@ -1,13 +1,40 @@
 package it.univpm.progetto_febbraio2020;
 
+import org.apache.catalina.Server;
+import org.slf4j.ILoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.number.PercentStyleFormatter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class ProgettoFebbraio2020Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ProgettoFebbraio2020Application.class, args);
+		String filename = "dataFile.tsv;";
+		String url = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/apro_mk_colm.tsv.gz&unzip=true";
+		
+		if(Files.exists(Paths.get(filename))) {	//Controlla se il file esiste già
+			System.out.println("File già scaricato.");
+		}else {
+		Downloader.download(url,filename);
+		}
+		
+		TSVReader parser = new TSVReader(filename); //Parsing del file tsv
+		parser.parsing();
+
+	
 	}
 
 }
+
+
+
