36a1a66d5062e58b0c4bd6f064cc94c411a54335
matteotoma98 <matteotoma98@hotmail.it>
2020-02-12 14:40:30 +0100

    Aggiornamento file ProgettoFebbraio2020 con librerie e import
    dei servizi.

--- progetto/src/main/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020Application.java
+++ progetto/src/main/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020Application.java
@@ -12,7 +12,8 @@ import java.nio.file.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import it.univpm.progetto_febbraio2020.services.Downloader;
-
+import it.univpm.progetto_febbraio2020.services.MetaData;
+import it.univpm.progetto_febbraio2020.services.TSVReader;
 
 
 @SpringBootApplication
@@ -23,17 +24,18 @@ public class ProgettoFebbraio2020Application {
 		String filename = "dataFile.tsv;";
 		String url = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/apro_mk_colm.tsv.gz&unzip=true";
 		
-		if(Files.exists(Paths.get(filename))) {	//Controlla se il file esiste già
+		
+		if(Files.exists(Paths.get(filename))) {	//Controlla se il file esiste già, altrimenti lo scarica
 			System.out.println("File già scaricato.");
 		}else {
 		Downloader.download(url,filename);
 		}
 		
-		TSVReader parser = new TSVReader(filename); //Parsing del file tsv
+		TSVReader parser = new TSVReader(filename); //parser del file tsv
 		parser.parsing();
-	
+		MetaData metadata = new MetaData(); //crea la mappa per i metadati
+		
 	}
-
 }
 
 
