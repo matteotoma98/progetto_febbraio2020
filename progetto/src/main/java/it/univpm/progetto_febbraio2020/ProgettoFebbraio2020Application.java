a7eb237ebcd5eb675c67a2d9312fec82344eaa67
matteotoma98 <matteotoma98@hotmail.it>
2020-02-12 17:12:02 +0100

    Modifiche al file

--- progetto/src/main/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020Application.java
+++ progetto/src/main/java/it/univpm/progetto_febbraio2020/ProgettoFebbraio2020Application.java
@@ -15,27 +15,35 @@ import it.univpm.progetto_febbraio2020.services.Downloader;
 import it.univpm.progetto_febbraio2020.services.MetaData;
 import it.univpm.progetto_febbraio2020.services.TSVReader;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ProgettoFebbraio2020Application.
+ * @author Matteo e Federico
+ */
 @SpringBootApplication
 public class ProgettoFebbraio2020Application {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(ProgettoFebbraio2020Application.class, args);
-		String filename = "dataFile.tsv;";
-		String url = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/apro_mk_colm.tsv.gz&unzip=true";
-		
-		
-		if(Files.exists(Paths.get(filename))) {	//Controlla se il file esiste già, altrimenti lo scarica
-			System.out.println("File già scaricato.");
-		}else {
-		Downloader.download(url,filename);
-		}
-		
-		TSVReader parser = new TSVReader(filename); //parser del file tsv
-		parser.parsing();
-		MetaData metadata = new MetaData(); //crea la mappa per i metadati
-		
-	}
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     * @throws Exception the exception
+     */
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ProgettoFebbraio2020Application.class, args);
+        String filename = "datafile.tsv;";
+        String url = "http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/apro_mk_colm.tsv.gz&unzip=true";
+
+
+        if (Files.exists(Paths.get(filename))) { //Controlla se il file esiste già, altrimenti lo scarica
+            System.out.println("File già scaricato.");
+        } else {
+            Downloader.download(url, filename);
+        }
+
+        TSVReader parser = new TSVReader(filename); //parser del file tsv
+        parser.parsing();
+        MetaData metadata = new MetaData(); //crea la mappa per i metadati
+
+    }
 }
-
-
-
