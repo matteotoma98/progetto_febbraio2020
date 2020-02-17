4bca721367af7044f594dc50ac2bde841a8414c5
Federico <Federico@172.23.131.252>
2020-02-17 17:13:38 +0100

    Aggiunta classe Metadata

---
+++ progetto/src/main/java/it/univpm/progetto_febbraio2020/services/MetaData.java
@@ -0,0 +1,44 @@
+package it.univpm.progetto_febbraio2020.services;
+import it.univpm.progetto_febbraio2020.models.DairyStatus;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+// TODO: Auto-generated Javadoc
+
+/**
+ * The Class MetaData.
+ * @author Matteo e Federico
+ */
+public class MetaData {
+
+    /** The metalist. */
+    private List < Map > metalist = new ArrayList < > ();
+
+    /**
+     * Gets the metalist.
+     *
+     * @return the metalist
+     */
+    public List < Map > getMetalist() {
+        return metalist;
+    }
+
+    /**
+     * Instantiates a new meta data.
+     */
+    public MetaData() {
+
+        Field[] campi = DairyStatus.class.getDeclaredFields(); //importa gli attributi dalla classe main
+        for (Field i: campi) {
+            Map < String, String > map = new HashMap < > ();
+            map.put("alias", i.getName());
+            if (!i.getName().equals("dairy"))
+                map.put("source", i.getName().toUpperCase());
+            else map.put("source", "TIME");
+            map.put("type", i.getType().getSimpleName());
+            metalist.add(map);
+            }
+    }
+}
